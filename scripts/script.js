c99f8c605075013e93fa477656341efea078067c
juan <jlfloresjr@gmail.com>
2017-11-24 18:06:22 -0800

    backtracked a bit

--- scripts/script.js
+++ scripts/script.js
@@ -1,42 +1,40 @@
 (function () {
-    
+    // alert("What it do?!");
     var app = angular.module('rebornApp', []);
     
         app.controller('RebornController', function () {
-            //
+            alert("What it do?!");
         });
 
         // app.controller('CarouselController', function () {
         //     this.items = carousel;
         // });
 
-        angular.module('app', ['ui.bootstrap']);
-        function CarouselDemoCtrl($scope){
-          $scope.myInterval = 3000;
-          $scope.slides = [
-            {
-                image: 'images\temp-pic-kitchen-2.png'
-            },
-            {
-                image: 'images\temp-pic-kitchen.jpg'
-            },
-            {
-                image: 'images\temp-pic-tile-setter.jpg'
-            }
-          ];
-        }
+        // angular.module('app', ['ui.bootstrap']);
+        // function CarouselDemoCtrl($scope){
+        //   $scope.myInterval = 3000;
+        //   $scope.slides = [
+        //     {
+        //         image: 'images\temp-pic-kitchen-2.png'
+        //     },
+        //     {
+        //         image: 'images\temp-pic-kitchen.jpg'
+        //     },
+        //     {
+        //         image: 'images\temp-pic-tile-setter.jpg'
+        //     }
+        //   ];
+        // }
 
-        //
-
-        var carousel = [
-            {
-                image: 'images\temp-pic-kitchen-2.png'
-            },
-            {
-                image: 'images\temp-pic-kitchen.jpg'
-            },
-            {
-                image: 'images\temp-pic-tile-setter.jpg'
-            }
-        ]
+        // var carousel = [
+        //     {
+        //         image: 'images\temp-pic-kitchen-2.png'
+        //     },
+        //     {
+        //         image: 'images\temp-pic-kitchen.jpg'
+        //     },
+        //     {
+        //         image: 'images\temp-pic-tile-setter.jpg'
+        //     }
+        // ]
 })();
