ce0a1dd7c6fe833eabff215fa431d1c36bc2c9dc
avek1 <jlfloresjr@gmail.com>
2018-07-20 11:54:39 -0700

    simplified index-v3

--- scripts/script.js
+++ scripts/script.js
@@ -4,8 +4,8 @@ $(document).ready(function () {
     $("#photoGalleryBtn").on("click", reborn.togglePhototGallery);
     $("#testGalleryBtn").on("click", reborn.backToMain);
     reborn.runLightBoxGallery();
+    reborn.runFilteredGallery();
 
-  
 
 
 });
@@ -16,42 +16,95 @@ reborn.togglePhototGallery = function (event) {
     $(".test-gallery").removeClass("hidden");
 }
 
-reborn.backToMain = function(){
+reborn.backToMain = function () {
     $(".test-gallery").addClass("hidden");
     $(".main-page").removeClass("hidden");
 }
 
-reborn.runLightBoxGallery = function(){
-  /* activate the carousel */
-  $("#modal-carousel").carousel({ interval: false });
+reborn.runLightBoxGallery = function () {
+    /* activate the carousel */
+    $("#modal-carousel").carousel({ interval: false });
 
-  /* change modal title when slide changes */
-  $("#modal-carousel").on("slid.bs.carousel", function () {
-      $(".modal-title")
-          .html($(this)
-              .find(".active img")
-              .attr("title"));
-  });
+    /* change modal title when slide changes */
+    $("#modal-carousel").on("slid.bs.carousel", function () {
+        $(".modal-title")
+            .html($(this)
+                .find(".active img")
+                .attr("title"));
+    });
 
-  /* when clicking a thumbnail */
-  $(".row .thumbnail").click(function () {
-      var content = $(".carousel-inner");
-      var title = $(".modal-title");
+    /* when clicking a thumbnail */
+    $(".row .thumbnail").click(function () {
+        var content = $(".carousel-inner");
+        var title = $(".modal-title");
 
-      content.empty();
-      title.empty();
+        content.empty();
+        title.empty();
 
-      var id = this.id;
-      var repo = $("#img-repo .item");
-      var repoCopy = repo.filter("#" + id).clone();
-      var active = repoCopy.first();
+        var id = this.id;
+        var repo = $("#img-repo .item");
+        var repoCopy = repo.filter("#" + id).clone();
+        var active = repoCopy.first();
 
-      active.addClass("active");
-      title.html(active.find("img").attr("title"));
-      content.append(repoCopy);
+        active.addClass("active");
+        title.html(active.find("img").attr("title"));
+        content.append(repoCopy);
 
-      // show the modal
-      $("#modal-gallery").modal("show");
-  });
+        // show the modal
+        $("#modal-gallery").modal("show");
+    });
+
+}
+
+
+reborn.runFilteredGallery = function () {
+    console.log("this is the filtered gallery function");
+    filterSelection("all") // Execute the function and show all columns
+    function filterSelection(c) {
+        var x, i;
+        x = document.getElementsByClassName("column");
+        if (c == "all") c = "";
+        // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+        for (i = 0; i < x.length; i++) {
+            w3RemoveClass(x[i], "show");
+            if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+        }
+    }
+
+    // Show filtered elements
+    function w3AddClass(element, name) {
+        var i, arr1, arr2;
+        arr1 = element.className.split(" ");
+        arr2 = name.split(" ");
+        for (i = 0; i < arr2.length; i++) {
+            if (arr1.indexOf(arr2[i]) == -1) {
+                element.className += " " + arr2[i];
+            }
+        }
+    }
+
+    // Hide elements that are not selected
+    function w3RemoveClass(element, name) {
+        var i, arr1, arr2;
+        arr1 = element.className.split(" ");
+        arr2 = name.split(" ");
+        for (i = 0; i < arr2.length; i++) {
+            while (arr1.indexOf(arr2[i]) > -1) {
+                arr1.splice(arr1.indexOf(arr2[i]), 1);
+            }
+        }
+        element.className = arr1.join(" ");
+    }
+
+    // Add active class to the current button (highlight it)
+    var btnContainer = document.getElementById("myBtnContainer");
+    var btns = btnContainer.getElementsByClassName("btn");
+    for (var i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function () {
+            var current = document.getElementsByClassName("active");
+            current[0].className = current[0].className.replace(" active", "");
+            this.className += " active";
+        });
+    }
 
 }
